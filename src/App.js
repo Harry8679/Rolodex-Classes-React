d2befb513be392762261e83fb4197a68c317e499
Harry EMARH <emarh.harry.code@gmail.com>
2024-11-18 23:32:32 +0100

    filter 2

--- src/App.js
+++ src/App.js
@@ -23,9 +23,10 @@ class App extends Component {
     return (
       <div className="App">
         <input type="search" className='search-box' placeholder='Search Monsters' onChange={(event) => {
-          console.log(event.target.value);
+          console.log(event.target.value); 
+          const searchString = event.target.value.toLocaleLowerCase();
           const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.includes(event.target.value);
+            return monster.name.toLocaleLowerCase().includes(searchString);
           });
           this.setState(() => {
             return { monsters: filteredMonsters }
